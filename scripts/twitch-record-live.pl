26cf1b0736a2e2b9b7e2e6de8e6ade369856bbc0
Max Maischein <corion@corion.net>
2022-08-06 08:47:14 +0200

    Add caching so we don't hit Twitch unless we need it

--- scripts/twitch-record-live.pl
+++ scripts/twitch-record-live.pl
@@ -7,20 +7,29 @@ use WWW::Twitch;
 use Getopt::Long;
 use Pod::Usage;
 use POSIX 'strftime';
+use YAML 'LoadFile';
 
 GetOptions(
     'directory|d=s' => \my $stream_dir,
     'dl=s' => \my $youtube_dl,
     'quiet|q' => \my $quiet,
+    'max-stale|s=s' => \my $maximum_stale_seconds,
+    'channel-id|i=s' => \my $channel_id,
+    'config|f=s' => \my $config,
 ) or pod2usage(2);
 
 $stream_dir //= '.';
 $youtube_dl //= 'youtube-dl';
+$maximum_stale_seconds //= 15;
+$config //= 'twitch-record-live.yml';
+if( -f $config ) {
+    $config = LoadFile( $config )
+} else {
+    $config = {}
+}
 
 my $twitch = WWW::Twitch->new();
-
 my $channel = $ARGV[0];
-my $info = $twitch->live_stream($channel);
 
 sub info( $msg ) {
     if( ! $quiet ) {
@@ -28,34 +37,55 @@ sub info( $msg ) {
     }
 }
 
+sub get_channel_live_info( $channel ) {
+    state $info = $twitch->live_stream($channel);
+    return $info
+}
+
+sub get_channel_id( $channel ) {
+    state $id;
+    $id //= $channel_id
+        //  $config->{channels}->{$channel}
+        // do {
+            (get_channel_live_info($channel) // {})->{id}
+           }
+}
+
+sub stream_recordings( $directory, $streamname=$channel ) {
+    my $id = get_channel_id( $channel );
+    opendir my $dh, $stream_dir
+        or die "$stream_dir: $!";
+
+    my @recordings = grep { /\b$id\b.*\.mp4(\.part)?$/ }
+                     readdir $dh;
+}
+
 if( ! -d $stream_dir ) {
     die "Stream directory '$stream_dir' was not found: $!";
 };
 
-if( $info ) {
-    my $id = $info->{id};
+# If we have stale recordings, maybe our network went down
+# in between
+my $stale = $maximum_stale_seconds / (24*60*60);
 
-    opendir my $dh, $stream_dir
-        or die "$stream_dir: $!";
+# Wait for a file to be updated
+my @current = grep { -M $_ < $stale }
+              stream_recordings( $stream_dir, $channel );
 
-    # If we have stale recordings, maybe our network went down
-    # in between
-    my $one_minute = 24/60/60;
-    my @recordings = grep { /\b$id\b.*\.mp4(\.part)?$/ && -M $_ < $one_minute }
-                     readdir $dh;
-
-    @recordings = map { sprintf "%s (%s)", $_, strftime('%H:%M:%S', localtime(-M $_ * 24*60*60 ))} @recordings;
-
-    if( ! @recordings ) {
+# If we have a recent file, we are obviously still recording, no
+# need to hit Twitch
+if( ! @current ) {
+    # Check whether the channel is live
+    my $info = get_channel_live_info($channel);
+    if( $info ) {
+        my $id = $info->{id};
         info( "$channel is live (Stream $id)");
         info( "Launching $youtube_dl in $stream_dir" );
         chdir $stream_dir;
         exec $youtube_dl, '-q', "https://www.twitch.tv/$channel";
     } else {
-        info( "$channel is recording (@recordings)" );
-    };
-
+        info( "$channel is offline" );
+    }
 } else {
-    info( "$channel is offline" );
-}
-
+    info( "$channel is recording (@current)" );
+};
